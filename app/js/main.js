066d9f60f10eb35862e5148666161ea918890ec2
NikitaRuzhkov <nruzshkov@karazin.ua>
2023-08-22 18:18:49 +0300

    done full index

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,24 @@
 $(function(){
   
+
+  $('.print__items').slick({
+    slidesToShow: 3,
+  });
+  $('.reviews__inner').slick({
+    
+  });
+  $('.stock__items').slick({
+   
+  });
+  $('.reviews__star').rateYo({
+    starWidth: "12px",
+    ratedFill: "#5CD36C",
+    readOnly: true,
+  });
+
+  
+
+  var mixer = mixitup('.top-sale__items')
+
+
 });
