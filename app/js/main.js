fb20e773a0ad4a6db6b1e5b7cab7221e62d45cdd
NikitaRuzhkov <nruzshkov@karazin.ua>
2023-08-28 20:02:32 +0300

    added

--- app/js/main.js
+++ app/js/main.js
@@ -15,8 +15,23 @@ $(function(){
     ratedFill: "#5CD36C",
     readOnly: true,
   });
+  $('.shop-one__star').rateYo({
+    starWidth: "20px",
+    normalFill: "#F0F0F0",
+    ratedFill: "#FFC700",
+    readOnly: true,
+    
+  });
+  $('.reviews-one__info-star').rateYo({
+    starWidth: "20px",
+    normalFill: "#F0F0F0",
+    ratedFill: "#FFC700",
+    readOnly: true,
 
-  $('.shop-page__select').styler();
+  });
+  
+
+  $('.shop-page__select,.shop-one__select,.shop-one__input').styler();
 
   var mixer = mixitup('.top-sale__items')
 
